be2539e82c46d8a150be3e46026006c3439ab114
Julian Karl Bauer <juliankarlbauer@gmx.de>
2023-08-14 14:41:36 +0200

    Copy s02

---
+++ s03.py
@@ -0,0 +1,27 @@
+# Check "Extending pandas" documentation
+import pandas as pd
+import numpy as np
+
+from s01 import count_5s
+
+
+@pd.api.extensions.register_series_accessor("counter")
+class CountersAccessor:
+    def __init__(self, series):
+        self._series = series
+
+    def count_5s(self):
+        return self._series.apply(count_5s)
+
+
+series = pd.Series([1, 2, 5, 505])
+series = series.counter.count_5s()
+print(series)
+
+my_series = pd.Series(np.random.randint(0, 999, size=10_000_000))
+
+# # To time the execution on the larger "my_series" data,
+# # start ipython-session and enter the following command:
+# %timeit my_series.counter.count_5s()
+# np_internals = series._data
+# print()

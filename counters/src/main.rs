f2a8510d23cc7a175def2fc3a8e11af7deaee70d
Julian Karl Bauer <juliankarlbauer@gmx.de>
2023-08-14 14:29:23 +0200

    Follow solution again

--- counters/src/main.rs
+++ counters/src/main.rs
@@ -1,7 +1,15 @@
-fn main() {
-    println!("{}", count_5s(505));
-}
-
 fn count_5s(number: i64) -> usize {
     number.to_string().matches('5').count()
+}
+
+fn count_5s_vec(values: Vec<i64>) -> Vec<usize> {
+    values.into_iter()
+        .map(count_5s)
+        .collect()
+}
+
+fn main() {
+    println!("{}", count_5s(505));
+    let data = vec![1, 2, 5, 505];
+    println!("{:?}", count_5s_vec(data));
 }
